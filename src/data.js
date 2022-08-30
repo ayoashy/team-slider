7fb72725f53638c11efe683970e2ecdcb9976527
Topboyshoks <ashiruayo@gmail.com>
2022-08-30 15:20:07 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmxhY2slMjBwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    name: 'isiah james',
+    title: 'backend developer',
+    quote:
+      'The team lead backend developer,very hardworking and the best chess player in the team ',
+  },
+  {
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1523824921871-d6f1a15151f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmxhY2slMjBwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    name: 'rebecca daniels',
+    title: 'technical writer',
+    quote:
+      'always jovial and fun to be around with. she is never too tired bto write',
+  },
+  {
+    id: 3,
+    image:
+      'https://images.unsplash.com/photo-1529688530647-93a6e1916f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmxhY2slMjBwZXJzb258ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'senior product designer. not best friends with the frontend developers ',
+  },
+  {
+    id: 4,
+    image:
+      'https://images.unsplash.com/photo-1495603889488-42d1d66e5523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJsYWNrJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60',
+    name: 'steven moha',
+    title: 'the boss',
+    quote:
+      'full stack developer always ensuring everyone in the team are doing their job. the boss for a reason',
+  },
+  {
+    id: 5,
+    image:
+      'https://images.unsplash.com/photo-1507038732509-8b1a9623223a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGJsYWNrJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60',
+    name: 'Umar shoki',
+    title: 'frontend developer',
+    quote: 'lead frontend developer also a footballer lover   ',
+  },
+]
+
+export default people
